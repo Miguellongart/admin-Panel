1fb3390bcfd9c8a9cf8ed107ca1008aa65630e19
Miguel Longart <longart86@gmail.com>
2023-01-29 19:21:25 -0300

    seeders

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # admin-Panel
 Admin en laravel Brazze con livewire user-rol-permission
+
+1)- Clonar 
+2)- Copiar .env_example
+3)- composer install
+4)- crear base de datos
+5)- php artisan migrate --seed (crea paises y provincias, roles y permisos, usuario y asigna rol)
--- database/seeders/ConfigSeeder.php
+++ database/seeders/ConfigSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
+use App\Models\Province;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,92 @@ class ConfigSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $path = json_decode(file_get_contents(public_path('jsons/countrys.json'), true));
+        foreach($path as $item){
+            $country = Country::create([
+                'nombre' => $item->nombre,
+                'name' => $item->name,
+                'nom' => $item->nom,
+                'iso2' => $item->iso2,
+                'iso3' => $item->iso3,
+                'phone_code' => $item->phone_code,
+            ]);
+
+            switch($country->nombre) {
+                case "Venezuela":
+                    $path = json_decode(file_get_contents(public_path('jsons/ve.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+                case "Brasil":
+                    $path = json_decode(file_get_contents(public_path('jsons/br.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+                case "Argentina":
+                    $path = json_decode(file_get_contents(public_path('jsons/arg.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+                case "Uruguay":
+                    $path = json_decode(file_get_contents(public_path('jsons/uru.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+                case "México":
+                    $path = json_decode(file_get_contents(public_path('jsons/mx.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+                case "Colombia":
+                    $path = json_decode(file_get_contents(public_path('jsons/col.json'), true));
+                    foreach($path as $item){
+                        Province::create([
+                            'nombre' => $item->subdivision_name,
+                            'name' => $item->subdivision,
+                            'iso2' => $item->code,
+                            'iso3' => $item->Local,
+                            'country_id' => $country->id
+                        ]);
+                    }
+                    break;
+            }
+            
+        }
     }
 }
